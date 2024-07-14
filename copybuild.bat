abf1a3a37a618e2083649acf3b0e44a07e1c2dd6
tobony <onni22@gmail.com>
2024-07-14 16:02:49 +0900

    for adding

--- copybuild.bat
+++ copybuild.bat
@@ -1,9 +1,9 @@
 @echo off
 set destination=.\version\
 set source1=src-tauri\target\release\eeSteam.exe
-set source2=src-tauri\target\release\bundle\msi\eeSteam_0.1.0_x64_en-US.msi
-set source3=src-tauri\target\release\bundle\nsis\eeSteam_0.1.0_x64-setup.exe
+set source2=src-tauri\target\release\bundle\msi\
+set source3=src-tauri\target\release\bundle\nsis\
 
 xcopy /y %source1% %destination%
-xcopy /y %source2% %destination%
-xcopy /y %source3% %destination%
+xcopy /y %source2%eesteam_*.msi %destination%
+xcopy /y %source3%eesteam_*-setup.exe %destination%
